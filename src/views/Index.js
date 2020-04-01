3d03fb9526fce278c1cd7be846f0650ed3d06cf0
Chirag Bansal <chirag.bansal@rivigo.com>
2020-04-02 00:12:06 +0530

    Popup header

--- src/views/Index.js
+++ src/views/Index.js
@@ -62,56 +62,72 @@ class Index extends React.Component {
     return (
         <>
           <Popup defaultOpen closeOnEscape closeOnDocumentClick position="right center"
-                 contentStyle={{borderRadius: "0.375rem"}}>
+                 contentStyle={{borderRadius: "0.375rem", minWidth: "50%", width: "unset"}}
+                 className="col-md-6">
             {close => (
-                <CardBody className="pre-scrollable">
-                  <Row className="justify-content-end">
-                    <a className="close" href="#index" onClick={close}>
-                      &times;
-                    </a>
-                  </Row>
-                  <Row className="justify-content-center">
-                    {this.state.activeForm === 1 ?
-                        <VolunteerRegistration/> :
-                        this.state.activeForm === 2 ?
-                            <SeniorCitizenRegistration/> :
-                            <Nav pills horizontal>
-                              <NavItem>
-                                <NavLink
-                                    className="py-2 px-3 text-white bg-primary"
-                                    href="#"
-                                    onClick={e => {
-                                      this.setState({activeForm: 1});
-                                      e.preventDefault();
-                                    }}
-                                >
-                                  <span className="d-md-block">I want to help</span>
-                                </NavLink>
-                              </NavItem>
-                              <NavItem>
-                                <NavLink
-                                    className="py-2 px-3 text-white bg-primary"
-                                    href="#"
-                                    onClick={e => {
-                                      this.setState({activeForm: 2});
-                                      e.preventDefault();
-                                    }}
-                                >
-                                  <span className="d-md-block">I need help</span>
-                                </NavLink>
-                              </NavItem>
-                              <NavItem>
-                                <NavLink
-                                    className="py-2 px-3 text-white bg-primary"
-                                    href="#"
-                                    onClick={close}>
-                                  <span className="d-md-block">Just exploring</span>
-                                </NavLink>
-                              </NavItem>
-                            </Nav>
-                    }
-                  </Row>
-                </CardBody>
+                <>
+                  <CardHeader className="bg-transparent">
+                    <Row className="justify-content-end">
+                      <a className="close" href="#index" onClick={close}>
+                        &times;
+                      </a>
+                    </Row>
+                    <Row className="align-items-center">
+                      <div className="col text-center">
+                        <h2 className="mb-0">
+                          {this.state.activeForm === 1 ?
+                              'Volunteer Form' :
+                              this.state.activeForm === 2 ?
+                                  'Request Form' :
+                                  'Welcome to COVID SOS'}
+                        </h2>
+                      </div>
+                    </Row>
+                  </CardHeader>
+                  <CardBody className="pre-scrollable">
+                    <Row className="justify-content-center">
+                      {this.state.activeForm === 1 ?
+                          <VolunteerRegistration/> :
+                          this.state.activeForm === 2 ?
+                              <SeniorCitizenRegistration/> :
+                              <Nav pills horizontal>
+                                <NavItem>
+                                  <NavLink
+                                      className="py-2 px-3 text-white bg-primary"
+                                      href="#"
+                                      onClick={e => {
+                                        this.setState({activeForm: 1});
+                                        e.preventDefault();
+                                      }}
+                                  >
+                                    <span className="d-md-block">I want to help</span>
+                                  </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                  <NavLink
+                                      className="py-2 px-3 text-white bg-primary"
+                                      href="#"
+                                      onClick={e => {
+                                        this.setState({activeForm: 2});
+                                        e.preventDefault();
+                                      }}
+                                  >
+                                    <span className="d-md-block">I need help</span>
+                                  </NavLink>
+                                </NavItem>
+                                <NavItem>
+                                  <NavLink
+                                      className="py-2 px-3 text-white bg-primary"
+                                      href="#"
+                                      onClick={close}>
+                                    <span className="d-md-block">Just exploring</span>
+                                  </NavLink>
+                                </NavItem>
+                              </Nav>
+                      }
+                    </Row>
+                  </CardBody>
+                </>
             )}
           </Popup>
           <Header/>
